50bc6ac5e25a792fee9efb701b05a4d14141de48
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2019-05-01 12:28:10 +0100

    add sun component

---
+++ src/component/sun/sun.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./sun.style.scss";
+
+const Sun = () => {
+  return (
+    <div className="wrapper">
+      <div className="sun">
+        <div className="ray-box">
+          <div className="ray ray1" />
+          <div className="ray ray2" />
+          <div className="ray ray3" />
+          <div className="ray ray4" />
+          <div className="ray ray5" />
+          <div className="ray ray6" />
+          <div className="ray ray7" />
+          <div className="ray ray8" />
+          <div className="ray ray9" />
+          <div className="ray ray10" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sun;
--- src/weather-forecast-app.jsx
+++ src/weather-forecast-app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Cloud from "./component/cloud";
+import Sun from "./component/sun";
 
 const WeatherForecastApp = () => {
   return (
     <div className="content">
       <h1>Weather Forecast</h1>
+      <Sun />
       <Cloud />
     </div>
   );
