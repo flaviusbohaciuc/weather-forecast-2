993f3b3056c7a77c777a58a567c6198facb7f015
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2019-05-01 10:26:16 +0100

    add weather-forecast-app and clean create-react-app template

---
+++ src/weather-forecast-app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WeatherForecastApp = () => {
+  return (
+    <div>
+      <h1>
+        Weather Forecast
+      </h1>
+    </div>
+  );
+};
+
+export default WeatherForecastApp;
