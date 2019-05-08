8788ef47d37ff4a69e54ff2ae199eeb73d1812f9
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2019-05-08 14:27:15 +0100

    Updates

---
+++ precache-manifest.e79d1cf0a0067a2990e9b1856dc6471c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1788cb7a1a36bb7d0137f467bb22e0",
+    "url": "/weather-forecast-2/index.html"
+  },
+  {
+    "revision": "c72c295552e1fa502df8",
+    "url": "/weather-forecast-2/static/css/main.0c849641.chunk.css"
+  },
+  {
+    "revision": "4a706b8874a2a626e700",
+    "url": "/weather-forecast-2/static/js/2.8aa02bee.chunk.js"
+  },
+  {
+    "revision": "c72c295552e1fa502df8",
+    "url": "/weather-forecast-2/static/js/main.8b91d06a.chunk.js"
+  },
+  {
+    "revision": "550587abb1fae8a0412d",
+    "url": "/weather-forecast-2/static/js/runtime~main.611f2ab2.js"
+  }
+]);
